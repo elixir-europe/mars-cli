0dd516658d3dcdf615daeb694acc9c45fb7b03e4
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2025-09-17 12:56:32 +0200

    Remove all non-cli related files

--- repository-services/isajson-biosamples/src/main/java/com/elixir/biohackaton/ISAToSRA/biosamples/model/BiosampleAccessionsMap.java
+++
@@ -1,9 +0,0 @@
-package com.elixir.biohackaton.ISAToSRA.biosamples.model;
-
-import com.elixir.biohackaton.ISAToSRA.receipt.ReceiptAccessionsMap;
-
-public class BiosampleAccessionsMap {
-    public ReceiptAccessionsMap sourceAccessionsMap = new ReceiptAccessionsMap();
-    public ReceiptAccessionsMap studyAccessionsMap = new ReceiptAccessionsMap();
-    public ReceiptAccessionsMap sampleAccessionsMap = new ReceiptAccessionsMap();
-}
